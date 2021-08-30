8c0ae1fe975deba55057123de617fb8e265a2d97
kishan sharma <sbitt348@gmail.com>
2021-08-30 18:58:41 +0530

    kishan

--- lib/app/networking/interceptor.dart
+++ lib/app/networking/interceptor.dart
@@ -8,10 +8,6 @@ class CustomInterceptor extends Interceptor {
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    // await getToken();
-    // await ifTokenIsNull();
-    // options.headers["Authorization"] = "Bearer $accessToken";
-    // options.headers["x-request-id"] = 'request-id';
     super.onRequest(options, handler);
   }
 
@@ -24,36 +20,4 @@ class CustomInterceptor extends Interceptor {
   void onError(DioError err, ErrorInterceptorHandler handler) {
     super.onError(err, handler);
   }
-
-  // -----------------------------------------------------------------------------------------
-
-  // // getToken from getstorage
-  // getToken() async {
-  //   accessToken = storage.read('token');
-  // }
-
-  // // if token is null call logout function
-  // ifTokenIsNull({RequestOptions? options}) async {
-  //   if (accessToken == null || accessToken == "") {
-  //     if (options?.path != "/getOtp" && options?.path != "/login-mobile") {
-  //       await logout();
-  //     }
-  //   }
-  // }
-
-  // clear token from storage and then go to auth view
-  // logout() async {
-  //   await clearAccessTokenFromGetSTorage();
-  //   await goToAuthViewScreen();
-  // }
-
-  // // clearing getstoreage
-  // clearAccessTokenFromGetSTorage() async {
-  //   storage.erase();
-  // }
-
-  // // route to authView
-  // goToAuthViewScreen() {
-  //   getX.Get.toNamed(Routes.AUTH);
-  // }
 }
